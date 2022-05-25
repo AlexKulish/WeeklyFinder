0259d75d6ad7a0b186bcddfa4effc869a0b98bda
Alexey Kulish <alexkulish1993@gmail.com>
2022-05-25 17:45:35 +0300

    add logic for find weekday

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -16,8 +16,28 @@ class ViewController: UIViewController {
     
     @IBAction func findDayButtonTapped(_ sender: UIButton) {
         
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        
+        guard let day = dateTextField.text, let month = monthTextField.text, let year = yearTextField.text else { return }
+        
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let weekday = dateFormatter.string(from: date)
+        
+        resultDayLabel.text = weekday.capitalized
+        
     }
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
